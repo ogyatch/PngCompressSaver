055aa4c57f70bffa65b2cd45be836cc86c36d17d
ogyatch <selves.aglow_0k@icloud.com>
2023-12-08 09:23:37 +0900

    MV3に対応完了！

--- background.js
+++ background.js
@@ -1,71 +1,45 @@
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: "save-png",
-        title: chrome.i18n.getMessage("save_png"),
+        title: "Save PNG",
         contexts: ["image"]
     });
 
     chrome.contextMenus.create({
         id: "save-png-compressed",
-        title: chrome.i18n.getMessage("save_png_compressed"),
+        title: "Save PNG Compressed",
         contexts: ["image"]
     });
+
+    console.log("Context menus created");
 });
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    processImage(info.srcUrl, info.menuItemId === "save-png-compressed");
-});
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    console.log("Context menu clicked:", info, tab);
 
-function processImage(imageUrl, compress) {
-    fetch(imageUrl, { mode: 'cors' }).then(response => {
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.statusText}`);
+    if (info.menuItemId === "save-png" || info.menuItemId === "save-png-compressed") {
+        if (tab && tab.id) {
+            chrome.tabs.sendMessage(tab.id, {
+                action: "processImage",
+                imageUrl: info.srcUrl,
+                compress: info.menuItemId === "save-png-compressed"
+            });
+            console.log("Message sent to content script");
         }
-        return response.blob();
-    }).then(blob => {
-        var reader = new FileReader();
-        reader.onload = function () {
-            var img = new Image();
-            img.onload = function () {
-                var canvas = document.createElement('canvas');
-                var ctx = canvas.getContext('2d');
-                canvas.width = img.width;
-                canvas.height = img.height;
-                ctx.drawImage(img, 0, 0);
+    }
+});
 
-                if (compress) {
-                    // 圧縮処理
-                    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                    var compressedData = UPNG.encode([imageData.data.buffer], canvas.width, canvas.height, 256);
-                    console.log("圧縮データサイズ: ", compressedData.length); // 圧縮データサイズの出力
-                    var blob = new Blob([compressedData], { type: 'image/png' });
-                    console.log("圧縮後のBlobサイズ: ", blob.size); // 圧縮後のBlobサイズの出力
-                    saveImage(blob);
-                } else {
-                    // 非圧縮処理
-                    canvas.toBlob(function (blob) {
-                        saveImage(blob);
-                    }, 'image/png');
-                }
-            };
-            img.onerror = function (e) {
-                console.error("Image loading error: ", e);
-            };
-            img.src = reader.result;
-        };
-        reader.onerror = function (e) {
-            console.error("FileReader error: ", e);
-        };
-        reader.readAsDataURL(blob);
-    }).catch(error => {
-        console.error("Fetch error: ", error);
-    });
-}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Background script received message:", message);
 
-function saveImage(blob) {
-    var url = URL.createObjectURL(blob);
-    chrome.downloads.download({
-        url: url,
-        saveAs: true
-    });
-}
+    if (message.action === "downloadImage" && message.imageDataUrl) {
+        // Directly use the data URL for the download
+        chrome.downloads.download({
+            url: message.imageDataUrl,
+            filename: "downloaded_image.png", // You can give any name you like
+            saveAs: true // This will prompt the save dialog
+        }, (downloadId) => {
+            console.log("Download initiated, ID:", downloadId);
+        });
+    }
+});
